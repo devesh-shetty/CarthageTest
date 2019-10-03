341b2418e2c9711efa057a2adb7d9f4abf2772c1
Devesh Shetty <devesh.shetty007@gmail.com>
2019-10-02 21:34:03 -0400

    Initial Commit

---
+++ CarthageTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarthageTest
+//
+//  Created by Devesh Shetty on 2019-10-02.
+//  Copyright © 2019 Devesh Shetty. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
